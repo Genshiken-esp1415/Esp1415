496538ba689161c068ea012d28a8a9cd5270603f
Laura Quartesan <quarz92@gmail.com>
2015-05-14 00:29:36 +0200

    Per qualche motivo che ancora non ho capito bene crasha, per il resto: -aggiunto metodo per avere il numero di cadute della sessione nella classe Session, -creato layout per l'adapter, -aggiunto adapter personalizzato a ListaSessioniActivity. (ho di nuovo avuto problemi con la connessione quindi forse vengono push doppi, scusate ç_ç)

--- esp1415/src/it/unipd/dei/esp1415/ListaSessioniActivity.java
+++ esp1415/src/it/unipd/dei/esp1415/ListaSessioniActivity.java
@@ -1,7 +1,12 @@
 package it.unipd.dei.esp1415;
 
-import java.util.ArrayList;
+import it.unipd.dei.esp1415.R.drawable;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.ActionBarActivity;
@@ -11,10 +16,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-
-
-
+import android.widget.TextView;
 
 /**
  * Questa activity conterrà la lista delle sessioni.
@@ -59,34 +63,83 @@ public class ListaSessioniActivity extends ActionBarActivity {
 
 		public PlaceholderFragment() {
 		}
-
+		
+		ListView lista;
+		
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_lista_sessioni,
 					container, false);
 			
-			
-			/*commento a caso perché non mi fa fare il commit*/
-	
-			final ListView lista = (ListView) rootView.findViewById(R.id.list);
-			
-			//questo mi restituisce un arraylist di sessioni con dati casuali per testing
+			ListView lista = (ListView) rootView.findViewById(R.id.list);
+			//ListView lista = getListView();
+			// questo mi restituisce un arraylist di sessioni con dati casuali
+			// per testing
 			ArrayList<Session> provaRandomizer = Randomizer.randomSession(30);
-			while(true){
-				
-			}
-			
-			/* SimpleCursorAdapter da usare al posto di ArrayAdapter per gestire dati del database */
-			ArrayAdapter<Session> adapter = new ArrayAdapter<Session>(getActivity(),
-					android.R.layout.simple_list_item_1, provaRandomizer);
-			
-			lista.setListAdapter(adapter);
-		
+			/*while (true) {
 
-			
+			}*/
+
+			/* SimpleCursorAdapter da usare al posto di ArrayAdapter per gestire
+			 * dati del database
+			 */
+			MyAdapter adapter = new MyAdapter(getActivity().getBaseContext(), R.layout.adapter_lista_sessioni, 
+												provaRandomizer);
+			setListAdapter(adapter);
+
 			return rootView;
-			
+
 		}
+		
 	}
+
+	// classe per l'adapter
+	public static class MyAdapter extends ArrayAdapter<Session> {
+		//inizializzo a null altrimenti dice che le variabili potrebbe non essere inizializzate per via del 
+		//ciclo for
+		private final String[] session_name = null;
+		private final Date[] session_begin = null;
+		private final int[] duration = null;
+		private final int[] number_of_falls = null;
+		private final ImageView[] thumbail = null;
+		Context context;
+		Holder holder;
+
+		public MyAdapter(Context context, int textVewResourceId, ArrayList<Session> provaRandomizer)
+		{
+			super(context, textVewResourceId, provaRandomizer);
+			this.context = context;
+			for(int i=0; i<provaRandomizer.size(); i++)
+				{
+				this.session_name[i] = provaRandomizer.get(i).getName();
+				this.session_begin[i] = provaRandomizer.get(i).getSessionBegin();
+				this.duration[i] = provaRandomizer.get(i).getDuration();
+				this.number_of_falls[i] = provaRandomizer.get(i).getNumberOfFalls();
+				//this.thumbail[i] = sessioni[i].getQualcosa(); manca thumbail sessione
+				this.thumbail[i].setImageResource(R.drawable.thumbnail_placeholder);
+				}
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			holder = new Holder();
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		    View rowView = inflater.inflate(R.layout.adapter_lista_sessioni, parent, false);
+		    holder.textView = (TextView) rowView.findViewById(R.id.secondLine);
+		    holder.imageView = (ImageView) rowView.findViewById(R.id.thumbail);
+		    holder.textView2 = (TextView) rowView.findViewById(R.id.firstLine);
+		    //imposta i campi della prima e della seconda riga
+		    holder.textView2.setText(session_name[position]);
+		    holder.textView2.setText(session_begin[position] + " - " + duration[position] + " - " + 
+		    					number_of_falls[position] + " cadute");
+		    return rowView;
+		  	}
+		
+		class Holder {//holder serve a migliorare le prestazioni nello scrolling,
+			// vedi: http://developer.android.com/training/improving-layouts/smooth-scrolling.html#ViewHolder
+				TextView textView, textView2;
+				ImageView imageView;
+			}
+}
 }
