2a14f63942044da6dc48773da859860a3fa92c82
Marco Speronello <marco@speronello.com>
2015-05-15 19:23:28 +0200

    Modificata GraphView per supportare i grafici in ognuno dei 3 assi. Sistemati i layout di conseguenza

--- esp1415/src/it/unipd/dei/esp1415/DettaglioCadutaActivity.java
+++ esp1415/src/it/unipd/dei/esp1415/DettaglioCadutaActivity.java
@@ -88,22 +88,29 @@ public class DettaglioCadutaActivity extends ActionBarActivity {
 			int numeroCaduta = fall.getFallNumber();
 			Date dataCaduta = fall.getFallTimestamp();
 			ArrayList<AccelerometerData> accData = fall.getFallData();
-//			getActivity().setTitle("Caduta #"+numeroCaduta+" della sessione "+session.getName());   
-//			TextView data = (TextView) rootView.findViewById(R.id.data);
-//			TextView ora = (TextView) rootView.findViewById(R.id.ora);
-//			TextView notifica = (TextView) rootView.findViewById(R.id.notifica);
-//			TextView latitudine = (TextView) rootView.findViewById(R.id.latitudine);
-//			TextView longitudine = (TextView) rootView.findViewById(R.id.longitudine);
-//			data.setText(DateFormat.format("dd/MM/yy",dataCaduta));
-//			ora.setText(DateFormat.format("kk:mm:ss",dataCaduta));
-//			latitudine.setText(Double.toString(fall.getLatitude()));
-//			longitudine.setText(Double.toString(fall.getLongitude()));
-//			if(fall.isNotified())
-//				notifica.setText("inviata");
-//			else
-//				notifica.setText("non inviata");
-			GraphView g = (GraphView) rootView.findViewById(R.id.grafico_label);
-			g.setAccelerometerData(accData);
+			getActivity().setTitle("Caduta #"+numeroCaduta+" della sessione "+session.getName());   
+			TextView data = (TextView) rootView.findViewById(R.id.data);
+			TextView ora = (TextView) rootView.findViewById(R.id.ora);
+			TextView notifica = (TextView) rootView.findViewById(R.id.notifica);
+			TextView latitudine = (TextView) rootView.findViewById(R.id.latitudine);
+			TextView longitudine = (TextView) rootView.findViewById(R.id.longitudine);
+			data.setText(DateFormat.format("dd/MM/yy",dataCaduta));
+			ora.setText(DateFormat.format("kk:mm:ss",dataCaduta));
+			latitudine.setText(Double.toString(fall.getLatitude()));
+			longitudine.setText(Double.toString(fall.getLongitude()));
+			if(fall.isNotified())
+				notifica.setText("inviata");
+			else
+				notifica.setText("non inviata");
+			GraphView graphX = (GraphView) rootView.findViewById(R.id.grafico_x);
+			GraphView graphY = (GraphView) rootView.findViewById(R.id.grafico_y);
+			GraphView graphZ = (GraphView) rootView.findViewById(R.id.grafico_z);
+			graphX.setAccelerometerData(accData);
+			graphY.setAccelerometerData(accData);
+			graphZ.setAccelerometerData(accData);
+			graphX.setAxis(0);
+			graphY.setAxis(1);
+			graphZ.setAxis(2);
 			return rootView;
 		}
 	}
--- esp1415/src/it/unipd/dei/esp1415/GraphView.java
+++ esp1415/src/it/unipd/dei/esp1415/GraphView.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class GraphView extends View {
@@ -15,6 +16,8 @@ public class GraphView extends View {
 	private Paint paintX;
 	private Paint paintY;
 	private Paint paintZ;
+	private Paint paintLine;
+	private int axis;
 	private ArrayList<AccelerometerData> accData;
 	
 	public GraphView(Context context, AttributeSet attrs) {
@@ -23,40 +26,65 @@ public class GraphView extends View {
 		
 		paintX = new Paint();
 		paintX.setStyle(Style.STROKE);
-	    paintX.setStrokeWidth(4);
+	    paintX.setStrokeWidth(1);
 	    paintX.setColor(Color.RED);
 	    
 	    paintY = new Paint();
 		paintY.setStyle(Style.STROKE);
-	    paintY.setStrokeWidth(4);
+	    paintY.setStrokeWidth(1);
 	    paintY.setColor(Color.GREEN);
 	    
 	    paintZ = new Paint();
 		paintZ.setStyle(Style.STROKE);
-	    paintZ.setStrokeWidth(4);
+	    paintZ.setStrokeWidth(1);
 	    paintZ.setColor(Color.BLUE);
 		
-		setBackgroundResource(R.drawable.box);
+	    paintLine = new Paint();
+		paintLine.setStyle(Style.STROKE);
+	    paintLine.setStrokeWidth(1);
+	    paintLine.setColor(Color.CYAN);
+	    
+	    setBackgroundResource(R.drawable.box);
+	    
 	}
 	
+	@Override
 	protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec){
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		
 		int width = getMeasuredWidth();
 	    int height = getMeasuredHeight();
-		setMeasuredDimension(width,height);
+		setMeasuredDimension(448,75);
 	}
 	
 	public void setAccelerometerData(ArrayList<AccelerometerData> accData){
 		this.accData=accData;
 	}
 	
+	public void setAxis(int axis){
+		this.axis=axis;
+	}
+
 	protected void onDraw(Canvas c){
-		super.onDraw(c);
-		
-	    for(int i=0;i<1000;i=i+2){
-	    	c.drawPoint(i+3,accData.get(i).getX()+75,paintX);
-	    	c.drawPoint(i+3,accData.get(i).getY()+75,paintY);
-	    	c.drawPoint(i+3,accData.get(i).getZ()+75,paintZ);
-	    }
+		if(accData != null){
+			super.onDraw(c);
+			
+			c.drawLine(0,37,480,37,paintLine);
+			
+			if(axis==0){
+		    	c.drawPoint(1,(accData.get(0).getX()+37)/2,paintX);
+			    for(int i=5;i<1000;i=i+4)
+			    	c.drawLine(i-4,(accData.get(i-4).getX()+37)/2,i,(accData.get(i).getX()+37)/2,paintX);
+			}else if(axis==1){
+				c.drawPoint(1,(accData.get(0).getY()+37)/2,paintY);
+			    for(int i=5;i<1000;i=i+4)
+			    	c.drawLine(i-4,(accData.get(i-4).getY()+37)/2,i,(accData.get(i).getY()+37)/2,paintY);
+			}else{
+				c.drawPoint(1,(accData.get(0).getZ()+37)/2,paintZ);
+				for(int i=5;i<1000;i=i+4)
+					c.drawLine(i-4,(accData.get(i-4).getZ()+37)/2,i,(accData.get(i).getZ()+37)/2,paintZ);
+			}
+		}else
+			Log.v("GraphView","Canvas set to NULL");
 	}
 }
